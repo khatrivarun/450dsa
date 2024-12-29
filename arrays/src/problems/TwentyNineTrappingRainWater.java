0946cb7078d5604c5e35a1b48cb865e73f9c48a1
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2024-12-29 13:35:18 +0530

    29/12/2024 - Arrays ID: 28

---
+++ arrays/src/problems/TwentyNineTrappingRainWater.java
@@ -0,0 +1,59 @@
+package problems;
+
+/*
+ * Problem Link: https://www.geeksforgeeks.org/problems/trapping-rain-water-1587115621/1
+ *
+ * Given an array arr[] with non-negative integers representing the height of blocks.
+ * If the width of each block is 1, compute how much water can be trapped between
+ * the blocks during the rainy season.
+ *
+ * Examples:
+ * Input: arr[] = [3, 0, 0, 2, 0, 4]
+ * Output: 10
+ *
+ * Input: arr[] = [7, 4, 0, 9]
+ * Output: 10
+ *
+ * Input: arr[] = [6, 9, 9]
+ * Output: 0
+ */
+
+public class TwentyNineTrappingRainWater {
+
+  public int trappingWater(int arr[]) {
+    int trappedWater = 0;
+
+    int leftPointer = 1, rightPointer = arr.length - 2;
+    int leftMaxBarrier = arr[leftPointer - 1], rightMaxBarrier = arr[rightPointer + 1];
+
+    // Was not able to crack this one
+    // Good infographic explanation here:
+    // https://www.geeksforgeeks.org/trapping-rain-water/#best-approach-using-two-pointers-on-time-and-o1-space
+    while (leftPointer <= rightPointer) {
+
+      rightMaxBarrier = Math.max(rightMaxBarrier, arr[rightPointer]);
+
+      if (leftMaxBarrier < rightMaxBarrier) {
+
+        trappedWater += Math.max(0, leftMaxBarrier - arr[leftPointer]);
+
+        leftMaxBarrier = Math.max(leftMaxBarrier, arr[leftPointer]);
+
+        leftPointer += 1;
+
+      } else {
+
+        trappedWater += Math.max(0, rightMaxBarrier - arr[rightPointer]);
+
+        rightMaxBarrier = Math.max(rightMaxBarrier, arr[rightPointer]);
+
+        rightPointer -= 1;
+
+      }
+
+    }
+
+    return trappedWater;
+  }
+
+}
